c1cd9868741e394d1cc2931d1895011a94d9c922
Gloria <lovinglikecrazy95@gmail.com>
2020-06-30 12:51:44 -0400

    Fixed all code sniff issues.

--- web/modules/custom/paragraphs_view_mode/src/Plugin/Field/FieldFormatter/ConfiguredViewModeFormatter.php
+++ web/modules/custom/paragraphs_view_mode/src/Plugin/Field/FieldFormatter/ConfiguredViewModeFormatter.php
@@ -4,6 +4,7 @@ namespace Drupal\paragraphs_view_mode\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'configured_view_mode_formatter' formatter.
@@ -19,6 +20,26 @@ use Drupal\Core\Field\FormatterBase;
 class ConfiguredViewModeFormatter extends FormatterBase {
 
   /**
+   * The entity repository service.
+   *
+   * @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface
+   */
+  protected $entityDisplayRepository;
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create(
+      $container,
+      $configuration,
+      $plugin_id,
+      $plugin_definition
+    );
+    $instance->entityDisplayRepository = $container->get('entity_display.repository');
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
@@ -27,7 +48,7 @@ class ConfiguredViewModeFormatter extends FormatterBase {
     // @see \Drupal\filter\Element\ProcessedText::preRenderText()
     foreach ($items as $delta => $item) {
       $field_config = $item->getDataDefinition()->getFieldDefinition();
-      $field_view_modes = \Drupal::service('entity_display.repository')->getViewModes($field_config->get('entity_type'));
+      $field_view_modes = $this->entityDisplayRepository->getViewModes($field_config->get('entity_type'));
       $elements[$delta] = [
         '#type' => 'processed_text',
         '#text' => $item->value === 'default' ? 'Default' : $field_view_modes[$item->value]['label'],
